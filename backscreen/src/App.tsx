0552e9556259abf77025fe8d9c7d81b66fcf71c5
gcp-development <bruno.silveira.backup@gmail.com>
2022-11-12 17:52:43 +0000

    baseline

---
+++ backscreen/src/App.tsx
@@ -0,0 +1,11 @@
+import HeaderComponent from "./components/Header";
+import FeaturesComponent from "./components/Features";
+
+const App = () => (
+	<>
+ 	<HeaderComponent/>
+	<FeaturesComponent/>
+	</>
+);
+
+export default App;
---
+++ backscreen/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  devServer: {
+    port: 8001,
+    open: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "backscreen",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Backscreen": "./src/App",
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
